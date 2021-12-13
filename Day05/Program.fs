17e9689cac95e822c2533c713a376fc2ccc7bde8
seanmartz <sean@seanmartz.com>
2021-12-13 16:54:43 -0500

    Day 05 part 2

--- Day05/Program.fs
+++ Day05/Program.fs
@@ -8,30 +8,30 @@ type ventPoint = { start: int * int; finish: int * int }
 let rec generatePointsBetween points pointList : list<(int * int)> =
     match points with
     | head :: tail ->
-        let xs =
-            if fst head.start < fst head.finish then
-                [ (fst head.start) .. (fst head.finish) ]
-            else
-                [ (fst head.finish) .. (fst head.start) ]
-
-        let ys =
-            if snd head.start < snd head.finish then
-                [ (snd head.start) .. (snd head.finish) ]
-            else
-                [ (snd head.finish) .. (snd head.start) ]
-
+        let xs = if fst head.start < fst head.finish then [ (fst head.start) .. (fst head.finish) ] else [ (fst head.finish) .. (fst head.start) ]
+        let ys = if snd head.start < snd head.finish then [ (snd head.start) .. (snd head.finish) ] else [ (snd head.finish) .. (snd head.start) ]
+        let isStraight = xs.Length = 1 || ys.Length = 1 
         let newPoints =
-            if xs.Length = 1 then
-                ((Array.create ys.Length xs.Head |> List.ofArray), ys)
-                ||> List.map2 (fun x y -> (x, y))
+            if isStraight then
+                if xs.Length = 1 then
+                    ((Array.create ys.Length xs.Head |> List.ofArray), ys)
+                    ||> List.map2 (fun x y -> (x, y))
+                else
+                    ((Array.create xs.Length ys.Head |> List.ofArray), xs)
+                    ||> List.map2 (fun y x -> (x, y))
             else
-                ((Array.create xs.Length ys.Head |> List.ofArray), xs)
-                ||> List.map2 (fun y x -> (x, y))
-
+                let xsOrdered = if fst head.start > fst head.finish then xs |> List.sortDescending else xs |> List.sort
+                let ysOrdered = if snd head.start > snd head.finish then ys |> List.sortDescending  else ys |> List.sort 
+                (xsOrdered, ysOrdered) ||> List.map2(fun x y -> (x, y))
+                
         generatePointsBetween tail (pointList @ newPoints)
     | [] -> pointList
 
-
+let xOrYMatch vp =
+    let (x1, y1) = vp.start
+    let (x2, y2) = vp.finish
+    x1 = x2 || y1 = y2
+    
 [<EntryPoint>]
 let main argv =
     let fullPath =
@@ -56,32 +56,25 @@ let main argv =
         (startPoints, finishPoints)
         ||> List.map2 (fun sP fP -> { start = sP; finish = fP })
 
-    let maxX =
-        ventData
-        |> List.map (fun vp -> [ fst vp.start ] @ [ fst vp.finish ])
-        |> List.concat
-        |> List.max
+    let straightLinesOnly = ventData |> List.filter xOrYMatch
+    
+    
 
-    let maxY =
-        ventData
-        |> List.map (fun vp -> [ snd vp.start ] @ [ snd vp.finish ])
-        |> List.concat
-        |> List.max
+    let straightLinePoints = generatePointsBetween straightLinesOnly []
+    
+    let diagonals = ventData |> List.except straightLinesOnly
+    
+    let diagonalPoints = generatePointsBetween diagonals []
 
-    let straightLinesOnly =
-        ventData
-        |> List.filter
-            (fun vp ->
-                let (x1, y1) = vp.start
-                let (x2, y2) = vp.finish
-                x1 = x2 || y1 = y2)
-
-    let fullPointList = generatePointsBetween straightLinesOnly []
-
-    let occurences = fullPointList
-                     |> List.countBy id
-                     |> List.filter (fun q -> (snd q) > 1)
+    let straightOverlaps = straightLinePoints 
+                        |> List.countBy id
+                        |> List.filter (fun q -> (snd q) > 1)
+                        
+    let diagonalOverlaps = (straightLinePoints @ diagonalPoints) 
+                        |> List.countBy id
+                        |> List.filter (fun q -> (snd q) > 1)
 
                             
-    printfn $"Points that overlap: %d{occurences.Length}"
+    printfn $"Points that overlap: %d{straightOverlaps.Length}"
+    printfn $"Points that overlap with Diagonals: %d{diagonalOverlaps.Length}"
     0 // return an integer exit code
