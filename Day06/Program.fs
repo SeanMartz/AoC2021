d72ca0a0681695fb4dbaf3d06aaf2bceaffa00c8
seanmartz <sean@seanmartz.com>
2021-12-14 17:32:17 -0500

    Day 06 part 02

--- Day06/Program.fs
+++ Day06/Program.fs
@@ -2,15 +2,37 @@
 
 open System.IO
 
+let reduceKeys (generationMap: Map<int, int64>) : Map<int, int64> =
+    generationMap
+    |> Map.toSeq
+    |> Seq.map (fun tpl -> (fst tpl - 1, snd tpl))
+    |> Map.ofSeq
 
-let rec dayPass (currentDay: int) (maxDays: int) (fish: seq<int>) : seq<int> =
-    match currentDay.Equals maxDays with
-    | true -> fish
-    | _ ->
-        dayPass (currentDay + 1) maxDays fish
-                                          |> Seq.map (fun f -> f - 1)
-                                          |> Seq.map (fun f -> if f = -1 then [ 8 ] @ [ 6 ] else [ f ])
-                                          |> Seq.concat
+let populate (generationMap: Map<int, int64>) : Map<int, int64> =
+    let newFishOpt = generationMap |> Map.tryFind -1
+    
+    let newFish =
+        match newFishOpt with
+        | Some v -> v
+        | None -> int64 0
+
+    generationMap
+    |> Map.change 8 (fun x ->
+            match x with
+            | Some value -> Some(value + newFish)
+            | None -> Some newFish)
+    |> Map.change 6 (fun x ->
+            match x with
+            | Some value -> Some(value + newFish)
+            | None -> Some newFish)
+    |> Map.remove -1
+
+let rec doFor (times:int) (fishMap: Map<int,int64>) :Map<int,int64> = 
+    match times.Equals 0 with
+    | true -> fishMap
+    | _ -> doFor (times-1) fishMap
+                            |> reduceKeys
+                            |> populate
 
 [<EntryPoint>]
 let main argv =
@@ -19,12 +41,21 @@ let main argv =
         Path.Combine(__SOURCE_DIRECTORY__, "data.txt")
 
     let puzzleInput = File.ReadLines(fullPath) |> List.ofSeq
+        
+    let initialLanternFish =
+        (puzzleInput |> Seq.head).Split(",")
+        |> Seq.ofArray
+        |> Seq.map int
+    
+    let fishMap = initialLanternFish |> Seq.countBy id |> Seq.map(fun v -> (fst v, int64 (snd v)) ) |> Map.ofSeq
+    
+    let eightyGen = doFor 80 fishMap
+    let twoFiftySixGen = doFor 256 fishMap
+    
+    let answerEighty = eightyGen |> Map.toSeq |> Seq.map (fun tpl -> snd tpl) |> Seq.sum
+    printfn $"Fish after 80 days %d{answerEighty}"
 
-    let lanternFish = (puzzleInput |> Seq.head).Split(",")
-                                                |> Seq.ofArray
-                                                |> Seq.map int
+    let answerTwoFiftySix = twoFiftySixGen |> Map.toSeq |> Seq.map (fun tpl -> snd tpl) |> Seq.sum
+    printfn $"Fish after 256 days %d{answerTwoFiftySix}"
 
-    let newLanternFish80 = dayPass 0 80 lanternFish
-
-    printfn $"Fish after 80 days %d{newLanternFish80 |> Seq.length}"
     0 // return an integer exit code
