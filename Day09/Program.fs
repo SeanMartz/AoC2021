bfcd16c4954c9a65942cbb2634e98761e665094d
seanmartz <sean@seanmartz.com>
2021-12-21 21:09:50 -0500

    Cleanup day 09 part 01

--- Day09/Program.fs
+++ Day09/Program.fs
@@ -11,32 +11,31 @@ let main argv =
     let fullPath = Path.Combine(__SOURCE_DIRECTORY__, "data.txt")
 
     let data = File.ReadLines(fullPath)
-               |> List.ofSeq
-               |> List.map(fun x -> x
+               |> Array.ofSeq
+               |> Array.map(fun x -> x
                                     |> Seq.toList
-                                    |> Array.ofSeq)
+                                    |> Array.ofSeq
+                                    |> Array.map string// string -> int conversion necessary otherwise i'll get the characters encoding number
+                                    |> Array.map int)
+
+  
     
     let mutable localizedLowPoints = List.empty
-    
-    for i, row in data |> Array.ofList |> Array.indexed do
-        for j, point in row |> Array.indexed do
-            let pointToLeft = if j <> 0 then Some row.[j-1] else None 
-            let pointToRight = if j <> row.Length - 1 then Some row.[j+1] else None
-            let rowAbove = if i <> 0 then Some data.[i-1] else None
-            let rowBelow = if i <> data.Length - 1 then Some data.[i+1] else None
-            let pointAbove = match rowAbove with
-                             | Some r -> Some r.[j]
-                             | _ -> None
-            let pointBelow = match rowBelow with
-                             | Some r -> Some r.[j]
-                             | _ -> None
-                             
-            if (pointToLeft.IsNone || point < pointToLeft.Value)
-               && (pointToRight.IsNone || point < pointToRight.Value)
-               && (pointAbove.IsNone ||  point < pointAbove.Value)
-               && (pointBelow.IsNone || point < pointBelow.Value)
-            then localizedLowPoints <- localizedLowPoints @ [point]
 
+    for i, row in data |> Array.indexed do
+        for j, point in row |> Array.indexed do
+            let pointToLeft =   if j <> 0 then Some row.[j-1] else None 
+            let pointToRight =  if j <> row.Length - 1 then Some row.[j+1] else None
+            let pointAbove =    if i <> 0 then Some data.[i-1].[j] else None
+            let pointBelow =    if i <> data.Length - 1 then Some data.[i+1].[j] else None
+              
+            if (pointToLeft.IsNone  || point <  pointToLeft.Value)
+               && (pointToRight.IsNone || point <  pointToRight.Value)
+               && (pointAbove.IsNone ||  point <  pointAbove.Value)
+               && (pointBelow.IsNone || point <  pointBelow.Value)
+            then
+                localizedLowPoints <- localizedLowPoints @ [point]
+    
     let answer = localizedLowPoints
                     |> List.map string
                     |> List.map int
