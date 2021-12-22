9e59df1c988d3db515050147f72a9de986106f68
seanmartz <sean@seanmartz.com>
2021-12-22 16:01:45 -0500

    refactor Day 09 to remove mutable variable

--- Day09/Program.fs
+++ Day09/Program.fs
@@ -1,9 +1,13 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System.IO
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+
+let getNeighborValues (x, y) floor =
+        [ (-1, 0); (1, 0); (0, -1); (0, 1) ]
+        |> List.map (fun (neighborX, neighborY) -> (x + neighborX, y + neighborY))
+        |> List.choose (fun location -> floor |> Map.tryFind location)
+    
+type FloorMap = Map<int *int, int>
 
 [<EntryPoint>]
 let main argv =
@@ -17,30 +21,22 @@ let main argv =
                                     |> Array.ofSeq
                                     |> Array.map string// string -> int conversion necessary otherwise i'll get the characters encoding number
                                     |> Array.map int)
-
-  
     
-    let mutable localizedLowPoints = List.empty
-
-    for i, row in data |> Array.indexed do
-        for j, point in row |> Array.indexed do
-            let pointToLeft =   if j <> 0 then Some row.[j-1] else None 
-            let pointToRight =  if j <> row.Length - 1 then Some row.[j+1] else None
-            let pointAbove =    if i <> 0 then Some data.[i-1].[j] else None
-            let pointBelow =    if i <> data.Length - 1 then Some data.[i+1].[j] else None
-              
-            if (pointToLeft.IsNone  || point <  pointToLeft.Value)
-               && (pointToRight.IsNone || point <  pointToRight.Value)
-               && (pointAbove.IsNone ||  point <  pointAbove.Value)
-               && (pointBelow.IsNone || point <  pointBelow.Value)
-            then
-                localizedLowPoints <- localizedLowPoints @ [point]
-    
-    let answer = localizedLowPoints
-                    |> List.map string
-                    |> List.map int
-                    |> List.map (fun x -> x + 1)
-                    |> List.sum
+    let floorMap =
+        [for i, row in data |> Array.indexed do
+            for j, point in row |> Array.indexed -> (i, j), point ]
+        |> Map.ofList
+        
+    let lowPoints =
+        floorMap
+        |> Map.toList
+        |> List.map(fun (location, value) -> (location, value), floorMap |> getNeighborValues location)
+        |> List.filter(fun (locAndVal, neighborValues) -> snd locAndVal < (neighborValues |> List.min ))
+        |> List.map(fun (locAndVal, _) -> (fst locAndVal, snd locAndVal))
+        
+    let answer = lowPoints
+                |> List.map(fun (_, value) -> value + 1)
+                |> List.sum
                  
     printfn $"Answer %d{answer}" 
     0 // return an integer exit code
